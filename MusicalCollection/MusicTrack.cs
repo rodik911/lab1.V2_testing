c1538d0c9976ef3f763d6cf819ffb0fe0c101b6d
dasha <329191-23>
2025-02-24 09:25:39 +0300

    Class MusicTrack id added

---
+++ MusicalCollection/MusicTrack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+public class MusicTrack
+{
+    public string Artist { get; set; }
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public int Year { get; set; }
+    public MusicTrack(string artist, string title, string genre, int year)
+    {
+        Artist = artist;
+        Title = title;
+        Genre = genre;
+        Year = year;
+    }
+    public override string ToString()
+    {
+        return $"{Artist} - {Title} ({Year}) [{Genre}]";
+    }
+}
